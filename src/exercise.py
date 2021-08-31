5edeed131ba9e1fd2f5ab89cf13a0096e9cde8d4
Jake Lee <jake.lee@fdmgroup.com>
2021-08-31 13:33:23 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    pw = input("Password?")
+    if (pw == "Caput Draconis"):
+        print("Welcome!")
+    else:
+        print("Off with you!")
 
 if __name__ == '__main__':
     main()
